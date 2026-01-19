a15b8b168935ad0ce0f249c9d43ecab56e23ebc3
JDevLogic <israelruales7@gmail.com>
2026-01-19 17:46:46 +0100

    Acabado el Ejer17.py

--- Ejer17.py
+++ Ejer17.py
@@ -2,3 +2,19 @@
 Ticket de compra
 
 Pide precio de 3 productos, muestra total, IVA y total con IVA """
+
+producto1 = int(input("Introduce el precio del primer producto: "))
+producto2 = int(input("Introduce el precio del segundo producto: "))
+producto3 = int(input("Introduce el precio del tercer producto: "))
+
+iva = 21
+
+productos_total = producto1 + producto2 + producto3
+
+iva_total = productos_total * (iva/100)
+
+precio_total = productos_total + iva_total
+
+print(f"El precio total de los 3 productos es {productos_total}€.")
+print(f"El precio del IVA es de {iva_total}€.")
+print(f"El precio total con IVA incluido es de {precio_total}€.")
