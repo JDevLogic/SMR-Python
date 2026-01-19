d3bcaa75210f844d11cf54d3a87a6c0a6eb9744d
JDevLogic <israelruales7@gmail.com>
2026-01-19 21:23:20 +0100

    Acabado el Ejer20.py

--- Ejer20.py
+++ Ejer20.py
@@ -2,4 +2,14 @@
 Programa completo final
 
 Pide nombre, edad, horas estudio/semana. Calcula horas/mes y /año """
+sem_mes = 4
+sem_year = 52
 
+nombre = input("Introduce tu nombre: ")
+edad = int(input("Introduce tu edad: "))
+horas_semana = float(input("Cuantas horas semanales estudiaste? "))
+
+horas_mes = horas_semana * sem_mes
+horas_year = horas_semana * sem_year
+
+print(f"\nNombre: {nombre}.\nEdad: {edad}.\nLas horas de estudio al mes son: {horas_mes}.\nLas horas de estudio al año son: {horas_year}.")
