29a64c8e0fe8abd2c03c760667182fb8cda9c452
JDevLogic <israelruales7@gmail.com>
2026-01-19 17:42:11 +0100

    Acabado el Ejer19.py

--- Ejer19.py
+++ Ejer19.py
@@ -3,3 +3,10 @@ Conversor de monedas
 
 Pide euros, convierte a dólares (tipo fijo), muestra resultado """
 
+dolares = 1.16
+
+euros = int(input("Introduce cuantos euros tienes: "))
+
+dolares_total = euros * dolares
+
+print(f"En dolares tienes {dolares_total}$.")
