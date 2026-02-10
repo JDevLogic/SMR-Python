ca01b733f22ed15f214207338f18345a34f44499
JDevLogic <israelruales7@gmail.com>
2026-02-10 19:45:44 +0100

    Creado el ejercicio de condiciones (alquiler_coche.py)

---
+++ Condiciones/alquiler_coche.py
@@ -0,0 +1,10 @@
+"""
+Ejercicio 6 
+
+Un sistema de alquiler de coches decide si un cliente puede alquilar: Solicita edad, años de
+carnet y si tiene tarjeta de crédito. - Menor de 21: no puede alquilar. - Entre 21 y 24: necesita al
+menos 2 años de carnet. - A partir de 25: basta con 1 año de carnet. En todos los casos es
+obligatoria la tarjeta de crédito. Muestra si puede alquilar o no y el motivo. 
+
+"""
+
