7a0b0509c67cfedbf49e6ac5f7695ba85f1b6159
JDevLogic <israelruales7@gmail.com>
2026-02-11 20:56:05 +0100

    Acabado el ejercicio de condiciones (alquiler_coche.py)

--- Condiciones/alquiler_coche.py
+++ Condiciones/alquiler_coche.py
@@ -8,3 +8,28 @@ obligatoria la tarjeta de crédito. Muestra si puede alquilar o no y el motivo.
 
 """
 
+edad = int (input("Introduce tu edad: "))
+carnet = int (input("Introduce tus años de carnet: "))
+credito = input(f"Tienes tarjeta de credito?\n").lower()
+
+if credito == "si":
+    if edad < 21:
+        print("No puedes alquilar, porque tienes menos de 21 años.")
+    
+    elif edad >= 21 and edad <=24:
+        if carnet >= 2:
+            print("Puedes alquilar un coche, porque tienes entre 21 y 24 y tienes al menos 2 años de carnet")
+        else:
+            print("No puedes alquilar coche, porque no tienes al menos 2 años de carnet.")
+    
+    elif edad >= 25:
+        if carnet >= 1:
+            print("Puedes alquilar un coche, porque tienes a partir de 25 y al menos 1 año de carnet.")
+        else:
+            print("No puedes alquilar un coche, porque no tienes al menos un año de carnet")
+
+elif credito == "no":
+    print("No puedes alquilar un coche, porque no tienes tarjeta de credito.")
+
+else:
+    print("Escribe si tienes tarjeta de credito con Si o No.")
