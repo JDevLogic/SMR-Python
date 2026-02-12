1a065666dfaaa4fee7123f0152d8b08488e1e6a5
JDevLogic <israelruales7@gmail.com>
2026-02-12 07:25:10 +0100

    Creado el ejercicio de condicionales (tienda.py)

---
+++ Condiciones/tienda.py
@@ -0,0 +1,8 @@
+""" Ejercicio 9
+
+Una tienda online aplica descuentos: Solicita el total de la compra y si el cliente es socio. -
+Compras < 50 €: sin descuento. - Entre 50 y 150 €: 5%. - Más de 150 €: 10%. Los socios
+obtienen un 5% adicional. Muestra el precio final. 
+
+"""
+
