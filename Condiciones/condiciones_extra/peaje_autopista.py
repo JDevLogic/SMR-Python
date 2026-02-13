69f30eff3b0f96a9e6294c176257f0407bc4acfb
JDevLogic <israelruales7@gmail.com>
2026-02-13 22:32:44 +0100

    Ejercicio peaje_autopista.py enunciado

---
+++ Condiciones/condiciones_extra/peaje_autopista.py
@@ -0,0 +1,21 @@
+"""
+Ejercicio 2
+
+Peaje de autopista.
+Queremos calcular cuánto paga un vehículo al pasar por una autopista.
+El programa debe pedir:
+- tipo de vehículo (moto, coche, furgoneta o camión),
+- kilómetros recorridos (mayor que 0),
+- y la hora del paso (0–23).
+Cada vehículo tiene un precio distinto por kilómetro:
+- moto: 0.05 €/km
+- coche: 0.08 €/km
+- furgoneta: 0.10 €/km
+- camión: 0.15 €/km
+Además:
+- en horas punta (7–9 y 17–19) el precio sube un 25%;
+- si se recorren más de 200 km, hay un 10% de descuento, pero solo para coche y furgoneta.
+Muestra el total y explica si se aplicó recargo y/o descuento 
+
+"""
+
