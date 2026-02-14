b622b1df99f55381d94321d00205b23a96f2524f
JDevLogic <israelruales7@gmail.com>
2026-02-14 13:53:35 +0100

    Acabado el ejercicio de condicionales (peaje_autopista.py)

--- Condiciones/condiciones_extra/peaje_autopista.py
+++ Condiciones/condiciones_extra/peaje_autopista.py
@@ -19,3 +19,49 @@ Muestra el total y explica si se aplicó recargo y/o descuento
 
 """
 
+tipo = input("Introduce tu tipo de vehiculo (moto, coche, furgoneta o camión): ").lower()
+kilometros = int(input("Introduce tus kilometros recorridos (mayor que 0): "))
+hora = int(input("Introduce la hora del paso (0-23): "))
+
+moto = 0.05 
+coche = 0.08 
+furgoneta = 0.10 
+camion =  0.15 
+
+recargo = 0.25
+descuento = 0.10
+
+precio_base = 0
+
+print(f"\nElegiste el tipo de vehiculo {tipo}y recorriste {kilometros}km.\n")
+
+if tipo == "moto":
+    precio_base = moto * kilometros
+    precio_total = precio_base
+
+elif tipo == "coche":
+    precio_base = coche * kilometros
+    precio_total = precio_base
+
+elif tipo == "furgoneta":
+    precio_base = furgoneta * kilometros
+    precio_total = precio_base
+
+elif tipo == "camion" or tipo == "camión":
+    precio_base = camion * kilometros
+    precio_total = precio_base
+
+
+if (hora >= 7 and hora <= 9) or (hora >= 17 and hora <= 19):
+    precio_recar = precio_total * recargo
+    precio_total += precio_recar
+    print(f"TIENES UN RECARGO EN EL PRECIO del 25%")
+
+
+if kilometros > 200 and (tipo == "coche" or tipo == "furgoneta"):
+    desc = precio_total * descuento
+    precio_total -= desc
+    print(f"TIENES UN DESCUENTO EN EL PRECIO del 10%")
+
+
+print(f"\nTu precio total es de: {precio_total:.2f}€.")
