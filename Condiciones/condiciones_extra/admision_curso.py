f07df5ca8352e71ce71ca3870403c1b6c96fa535
JDevLogic <israelruales7@gmail.com>
2026-02-18 01:13:15 +0100

    Mejora del ejercicio de condicionales (admision_curso.py)

--- Condiciones/condiciones_extra/admision_curso.py
+++ Condiciones/condiciones_extra/admision_curso.py
@@ -29,27 +29,36 @@ nota_minima = 0
 
 if plazas == 0:
     print("\nRECHAZADO\n\nPorque no quedan las plazas. ")
+    exit()
 
-elif edad < 16:
+if edad < 16:
     print("\nRECHAZADO\n\nPorque eres menor de 16.")
+    exit()
 
 else:    
     if nivel == "ninguno":
         nota_minima = 70
+        print(f"Se aplica la nota minima a {nota_minima}, porque tu nivel es {nivel}.")
                 
     elif nivel == "basico" or nivel == "básico":
         nota_minima = 55
+        print(f"Se aplica la nota minima a {nota_minima}, porque tu nivel es {nivel}.")
 
     elif nivel == "intermedio":
         nota_minima = 40
+        print(f"Se aplica la nota minima a {nota_minima}, porque tu nivel es {nivel}.")
 
+    else:
+        print("Nivel no valido.")
+        exit()
 
     if edad >= 30:
         nota_minima -= 5
+        print(f"Se baja 5 puntos a la nota minima, porque tu edad es de 30 o más.")
 
         if nota_minima <35:
             nota_minima = 35
-
+            print("Tu nota minima se queda en 35 puntos, porque no puede bajar mas de 35.")
 
     if nota >= nota_minima:
         print(f"\nADMITIDO\n\nLa nota minima usada es de: {nota_minima}.\nTu nota es de: {nota}.")
